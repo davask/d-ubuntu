1d29f7c544889f204ac50f7687b19f61842573ba
davask <contact@davaskweblimited.com>
2016-03-18 11:10:57 +0100

    Update dwl-init.sh

--- dwl-init.sh
+++ dwl-init.sh
@@ -3,7 +3,8 @@ chmod -R 700 /tmp
 echo "List /tmp initialization files";
 ls -lah /tmp;
 echo 'Start initialization';
-for init in `ls /tmp | grep dwl-init- | sort -r`;do
+for init in `ls /tmp | grep dwl-init- | sort -r`;
+do
     echo "Initialization of /tmp/$init";
     do "/tmp/$init";
 done;
