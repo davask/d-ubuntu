be623cdad30f7e644bc021db9e0561dd53892a51
davask <davask.42@gmail.com>
2016-04-11 10:57:27 +0200

    Update dockerfile

--- dwl-init.sh
+++ dwl-init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-chmod -R 700 $DWL_INIT_DIR
+chmod -R 700 ${DWL_INIT_DIR}
 
 echo ">>>>> LIST /TMP INITIALIZATION FILES <<<<<";
-ls -lah $DWL_INIT_DIR | sort -r;
+ls -lah ${DWL_INIT_DIR} | sort -r;
 echo "";
 
 echo "##### START INITIALIZATION #####";
-for init in `ls $DWL_INIT_DIR | sort -r`;
+for init in `ls ${DWL_INIT_DIR} | sort -r`;
 do
-    echo ">>>>> Initialization of $DWL_INIT_DIR/$init <<<<<";
-    $DWL_INIT_DIR/$init;
+    echo ">>>>> Initialization of ${DWL_INIT_DIR}/${init} <<<<<";
+    ${DWL_INIT_DIR}/${init};
     echo "";
 done;
 unset DWL_INIT_COUNTER
@@ -17,11 +17,11 @@ unset DWL_INIT
 echo "##### END OF INITIALIZATION #####";
 echo "";
 
-echo ">>>>> ACCESS WORKDIR /home/${DWL_APP_USER} <<<<<";
-cd /home/${DWL_APP_USER}
+echo ">>>>> ACCESS WORKDIR ${DWL_USER_DIR} <<<<<";
+cd ${DWL_USER_DIR}
 
-echo ">>>>> SWITCH TO USER ${DWL_APP_USER} <<<<<";
-su ${DWL_APP_USER}
+echo ">>>>> SWITCH TO USER ${DWL_USER_NAME} <<<<<";
+su ${DWL_USER_NAME}
 echo "";
 
 if [ "${DWL_INIT}" = "app" ] || [ "${DWL_KEEP_ACTIVE}" = "true" ]; then
