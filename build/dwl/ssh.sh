a5a69863825ddba9292c47670581724dcb0e47fb
davask <contact@davaskweblimited.com>
2017-06-29 20:39:25 +0200

    Automatic update 16.04

--- build/dwl/ssh.sh
+++ build/dwl/ssh.sh
@@ -2,6 +2,9 @@
 
 if [ "${DWL_SSH_ACCESS}" = "true" ]; then
     sudo sed -i "s|^# AllowUsers username|AllowUsers ${DWLC_USER_NAME}|g" /etc/ssh/sshd_config
+    sudo sed -i "s|^# Match User username|Match User ${DWLC_USER_NAME}|g" /etc/ssh/sshd_config
+    # sudo sed -i "s|^#       ChrootDirectory %h|      ChrootDirectory %h|g" /etc/ssh/sshd_config
+    sudo sed -i "s|^#       PasswordAuthentication yes|      PasswordAuthentication yes|g" /etc/ssh/sshd_config
     echo "> Enable ssh";
     sudo service ssh start;
 else
