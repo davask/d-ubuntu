279becf1124490c2b2501a7c0911a57a6789e020
davask <contact@davaskweblimited.com>
2017-07-10 10:02:53 +0200

    Automatic update 16.04

--- build/dwl/permission.sh
+++ build/dwl/permission.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
+if [ -d /dwl/home/host/files ]; then
+    sudo rm -rdf /home/${DWL_USER_NAME}/files;
+    sudo ln -sf /dwl/home/host/files /home/${DWL_USER_NAME}/files;
+fi
+
 sudo chown -R ${DWLC_USER_NAME}:${DWLC_USER_NAME} /home/${DWLC_USER_NAME}/;
-sudo chown root:root /home/${DWLC_USER_NAME};
+# sudo chown root:root /home/${DWLC_USER_NAME};
